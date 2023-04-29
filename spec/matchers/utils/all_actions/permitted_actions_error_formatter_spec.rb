647ba45fa4acba9fe9ad301d283822108c095335
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-04-29 15:57:21 +0200

    Introduce Test Policies
    This will allow to test matcher error messages

--- spec/matchers/utils/all_actions/permitted_actions_error_formatter_spec.rb
+++ spec/matchers/utils/all_actions/permitted_actions_error_formatter_spec.rb
@@ -11,34 +11,14 @@ RSpec.describe Pundit::Matchers::Utils::AllActions::PermittedActionsErrorFormatt
     Pundit::Matchers::Utils::AllActions::PermittedActionsMatcher.new(policy)
   end
 
-  let(:policy_class) do
-    Class.new do
-      def self.name
-        'DummyPolicy'
-      end
-
-      def initialize(update, create)
-        @update = update
-        @create = create
-      end
-
-      def update?
-        @update
-      end
-
-      def create?
-        @create
-      end
-    end
-  end
-
-  let(:policy) { policy_class.new(true, false) }
+  let(:policy_class) { TestCreateUpdatePolicy }
+  let(:policy) { policy_class.new(update: true) }
 
   describe '#message' do
     subject(:message) { error_message_formatter.message }
 
     it 'includes missed actions in message' do
-      expect(message).to eq('DummyPolicy expected to have all actions permitted, but [:create] is forbidden')
+      expect(message).to eq('TestPolicy expected to have all actions permitted, but [:create] is forbidden')
     end
   end
 end
